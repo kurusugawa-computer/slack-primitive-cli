91ee797eaa6d1d86f52755b66bc22fe239a4c190
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2024-05-13 14:49:28 +0900

    `files.upload` : 内部で`files_upload_v2`を使うようにしました。 (#138)
    * "--channel_id"に変更
    
    * 月ごとに更新
    
    * 修正

--- slack_primitive_cli/command/files.py
+++ slack_primitive_cli/command/files.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 @click.command(name="files.upload", help="Uploads or creates a file. See https://api.slack.com/methods/files.upload ")
 @click.option("--token", envvar=TOKEN_ENVVAR, required=True, help=TOKEN_HELP_MESSAGE)
-@click.option("--channels", required=True, help="Comma-separated list of channel names or IDs where the file will be shared.")
+@click.option("--channel_id", required=True, help="Channel IDs where the file will be shared.")
 @optgroup.group("File contents", cls=RequiredMutuallyExclusiveOptionGroup)
 @optgroup.option("--file", help="File contents via multipart/form-data. If omitting this parameter, you must submit content.")
 @optgroup.option("--content", help="File contents via a POST variable. If omitting this parameter, you must provide a file.")
@@ -22,15 +22,15 @@ logger = logging.getLogger(__name__)
 @click.option("--thread_ts", help="Provide another message's ts value to upload this file as a reply.")
 @click.option("--title", help="Title of file.")
 @my_backoff
-def upload(token, channels, file, content, filename, filetype, initial_comment, thread_ts, title):  # noqa: ANN001, ANN201
+def upload(token, channel_id, file, content, filename, filetype, initial_comment, thread_ts, title):  # noqa: ANN001, ANN201
     set_logger()
     client = slack_sdk.WebClient(token=token)
 
     if filename is None and file is not None:
         filename = os.path.basename(file)
 
-    response = client.files_upload(
-        channels=channels,
+    response = client.files_upload_v2(
+        channel=channel_id,
         file=file,
         content=content,
         filename=filename,
